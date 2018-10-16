64924e84424ac0fca0494229abbfaa2e51bd89de
Stuart_Campbell <44214812+StuartCampbell-iv@users.noreply.github.com>
2018-10-16 19:51:51 -0400

    Include update

--- Portsmouth/mainInclude.php
+++ Portsmouth/mainInclude.php
@@ -1,6 +1,18 @@
 <?php
 
-/* 
+function DisplayImage($FileName,$Alt,$Width = "",$Height = "")
+{
+	echo"<img src= \"$FileName\" alt = \"$Alt\" width = 
+	\"$Width\" height = \"$Height\">";
+}
+
+function DisplayButton($BName,$Text,$FileName = "", $Alt = "")
+{
+	
+	echo" <button type= submit name =\"$BName\" class =\"$FileName\">\"$Text\"</button>";
+}
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
