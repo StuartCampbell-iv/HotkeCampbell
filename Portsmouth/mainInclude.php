889a91386a48ff89fa8494fead369b47fcf34549
matthewhotke <matthew12hotke@hotmail.com>
2018-10-16 19:53:53 -0400

    yoyoyoyoy

--- Portsmouth/mainInclude.php
+++ Portsmouth/mainInclude.php
@@ -1,8 +1,31 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function WriteHeader()
+{
+    echo <<<HTML
+        <!doctype html> 
+        <html lang = \"en\">
+        <head>
+                <meta charset = \"UTF-8\">
+                <title>Title</title>
+        </head>
+        <body>     
+
+        <h1 class="jumbotron">Portmouth Tavern<div class="form-control">
+        <button>Home</button>
+        <button>Menu</button>
+        <button>Events</button>
+        <button>About Us</button>
+        </div></h1>
+HTML;
+}
+
+function WriteFooter()
+{
+    echo "<h2>Footer</h2>";
+    echo "</body>\n</html>\n";
+}
+
+
+
 ?>
