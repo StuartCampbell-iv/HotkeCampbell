c1d9768f9e826e218e51f8dce9ee3c55fdc21c92
Stuart_Campbell <44214812+StuartCampbell-iv@users.noreply.github.com>
2018-10-16 19:28:26 -0400

    Update Portsmouth/index.php

--- Portsmouth/index.php
+++ Portsmouth/index.php
@@ -8,10 +8,15 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1 class="jumbotron">Portmouth Tavern<div class="form-control">
+        <button>Home</button>
+        <button>Menu</button>
+        <button>Events</button>
+        <button>About Us</button>
+        </div></h1>
+        
     </body>
 </html>
