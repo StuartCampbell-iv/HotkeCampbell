20aa9d175fe3833aa640e2f1bb6023af372a0de5
matthewhotke <matthew12hotke@hotmail.com>
2018-10-16 19:58:35 -0400

    junk test

--- Portsmouth/mainInclude.php
+++ Portsmouth/mainInclude.php
@@ -27,6 +27,12 @@ function WriteFooter()
     echo "</body>\n</html>\n";
 }
 
+function WriteFooter2()
+{
+    echo "<h2>Footer</h2>";
+    echo "</body>\n</html>\n";
+}
+
 function DisplayImage($FileName,$Alt,$Width = "",$Height = "")
 {
 	echo"<img src= \"$FileName\" alt = \"$Alt\" width = 
