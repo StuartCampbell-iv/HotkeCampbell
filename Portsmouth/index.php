535af8337a5d880fbd6b4907f1adefe148b76a8d
matthewhotke <matthew12hotke@hotmail.com>
2018-10-18 19:47:55 -0400

    Add images

--- Portsmouth/index.php
+++ Portsmouth/index.php
@@ -5,7 +5,12 @@ require_once 'mainInclude.php';
 /// Main
 WriteHeader();
 
-
+if (isset($_POST['f_MenuPage']))
+{
+    MenuPage();
+}
+else if (isset($_POST['f_EventsPage']))
+{
     EventsPage();
 }
 else if (isset($_POST['f_ContactUsPage']))
