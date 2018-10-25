0802816304783365427ea049da7b49a7fde94213
Stuart_Campbell <44214812+StuartCampbell-iv@users.noreply.github.com>
2018-10-24 23:10:05 -0400

    login work

--- Portsmouth/loginInclude.php
+++ Portsmouth/loginInclude.php
@@ -3,15 +3,17 @@
 function Login()
 {
    echo <<<HTML
+    <form
         <label for="username">UserName</label>
         <input type"text" placeholder="Enter Username" name="username"
-            required>
+            id="username" required>
             
-        <label for="pswd">Password</label>
-        <input type="password" placeholder="Enter Password" name="pswd"
-            required>
+        <label for="password">Password</label>
+        <input type="password" placeholder="Enter Password" name="password"
+            id="password" required>
     
-        <button type="submit" name"f_Login">Login</button>
+        <button type="submit" name"f_Login" id="login" onclick="validate()">Login</button>
+    <script src=loginJS.js></script>
 HTML;
         
  
